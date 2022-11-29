9c9a0d3ceedfbb3ac6edbf04c82073861c10f2a9
Faruk Aydogan <57232389+farukkaydogan@users.noreply.github.com>
2022-11-29 08:56:46 +0300

    Delete vendor directory

--- vendor/zircote/swagger-php/tests/Fixtures/PHP/Inheritance/ExtenedsBaseInterface.php
+++
@@ -1,7 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace OpenApi\Tests\Fixtures\PHP\Inheritance;
-
-interface ExtenedsBaseInterface extends BaseInterface
-{
-}
--- vendor/zircote/swagger-php/tests/Fixtures/Parser/AllTraits.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace OpenApi\Tests\Fixtures\Parser;
-
-trait AllTraits
-{
-    use AsTrait;
-    use \OpenApi\Tests\Fixtures\Parser\HelloTrait;
-}
